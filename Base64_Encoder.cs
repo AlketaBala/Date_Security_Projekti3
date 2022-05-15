7af86c0438cbac49e4bd4086bf4d5ee61628c7ca
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-05-15 23:26:36 +0200

    Update Base64_Encoder.cs

--- Base64_Encoder.cs
+++ Base64_Encoder.cs
@@ -50,7 +50,7 @@ namespace Base64EncoderDecoder
         private char block6btochar(byte b)
         {
             char[] Characters = new char[64]
-                    {   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                    {   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', //vendosja e disa prej ascii karaktereve
                         'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                         '0','1','2','3','4','5','6','7','8','9','+','/'};
 
