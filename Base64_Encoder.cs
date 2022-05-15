d2717c38ce4d25a67dfad24e9db7bb196298b87e
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-05-15 23:28:31 +0200

    Update Base64_Encoder.cs

--- Base64_Encoder.cs
+++ Base64_Encoder.cs
@@ -78,40 +78,35 @@ namespace Base64EncoderDecoder
                 {
                     plaintext2[i] = 0;
                 }
-            }
-
-            byte b1, b2, b3;
-            byte j, j1, j2, j3, j4;
-            byte[] block6b = new byte[block * 4];
-            char[] ciphertext = new char[block * 4];
-             for (int i = 0; i < block; i++)
+           switch (padding)
             {
-                b1 = plaintext2[i * 3];
-                b2 = plaintext2[i * 3 + 1];
-                b3 = plaintext2[i * 3 + 2];
-
-                j1 = (byte)((b1 & 252) >> 2);
-
-                j = (byte)((b1 & 3) << 4);
-                j2 = (byte)((b2 & 240) >> 4);
-                j2 += j;
-
-                j = (byte)((b2 & 15) << 2);
-                j3 = (byte)((b3 & 192) >> 6);
-                j3 += j;
-
-                j4 = (byte)(b3 & 63);
-
-                block6b[i * 4] = j1;
-                block6b[i * 4 + 1] = j2;
-                block6b[i * 4 + 2] = j3;
-                block6b[i * 4 + 3] = j4;
-
+                case 0: break;
+                case 1: ciphertext[block * 4 - 1] = '='; break;
+                case 2:
+                    ciphertext[block * 4 - 1] = '=';
+                    ciphertext[block * 4 - 2] = '=';
+                    break;
+                default: break;
             }
+            return ciphertext;
+        }
 
-            for (int i = 0; i < block * 4; i++)
+        private char block6btochar(byte b)
+        {
+            char[] Characters = new char[64]
+                    {   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', // vendosja e disa prej ascii karaktereve
+                        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                        '0','1','2','3','4','5','6','7','8','9','+','/'};
+
+            if ((b >= 0) && (b <= 63))
             {
-                ciphertext[i] = block6btochar(block6b[i]);
+                return Characters[(int)b];
             }
+            else
+            {
 
-            
+                return ' ';
+            }
+        }
+    }
+}
