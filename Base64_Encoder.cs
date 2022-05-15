0bea6ac6b5c028335b8c50592f7773f575948521
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-05-15 23:25:16 +0200

    Update Base64_Encoder.cs

--- Base64_Encoder.cs
+++ Base64_Encoder.cs
@@ -33,6 +33,40 @@ namespace Base64EncoderDecoder
         {
             byte[] plaintext2;
             plaintext2 = new byte[length2];
+        
+            switch (padding)
+            {
+                case 0: break;
+                case 1: ciphertext[block * 4 - 1] = '='; break;
+                case 2:
+                    ciphertext[block * 4 - 1] = '=';
+                    ciphertext[block * 4 - 2] = '=';
+                    break;
+                default: break;
+            }
+            return ciphertext;
+        }
+
+        private char block6btochar(byte b)
+        {
+            char[] Characters = new char[64]
+                    {   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                        '0','1','2','3','4','5','6','7','8','9','+','/'};
+
+            if ((b >= 0) && (b <= 63))
+            {
+                return Characters[(int)b];
+            }
+            else
+            {
+
+                return ' ';
+            }
+        }
+    }
+}
+
 
             for (int i = 0; i < length2; i++)
             {
