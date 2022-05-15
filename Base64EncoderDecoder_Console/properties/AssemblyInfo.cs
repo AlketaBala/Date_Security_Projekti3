a6841725739dd837466bb252c01470402c31886a
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-05-15 23:54:58 +0200

    Create AssemblyInfo.cs

---
+++ Base64EncoderDecoder_Console/properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Base64EncoderDecoder_Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Base64EncoderDecoder_Console")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee7cc973-7a2a-4acb-b43d-f8b9bb312584")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
