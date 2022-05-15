b0c8478ad69aa32cb840c6fc224fa539c608fa4a
Albert Grajçevci <94078877+albertgrajqevci@users.noreply.github.com>
2022-05-15 23:31:03 +0200

    Update Base64_Decoder.cs

--- Base64_Decoder.cs
+++ Base64_Decoder.cs
@@ -81,3 +81,25 @@ namespace Base64EncoderDecoder
 
             return plaintext;
         }
+       private byte chartoblock(char a)
+        {
+            char[] Characters = new char[64]
+                    {   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                        '0','1','2','3','4','5','6','7','8','9','+','/'};
+            if (a == '=')
+                return 0;
+            else
+            {
+                for (int i = 0; i < 64; i++)
+                {
+                    if (Characters[i] == a)
+                        return (byte)i;
+                }
+
+                return 0;
+            }
+
+        }
+    }
+}
