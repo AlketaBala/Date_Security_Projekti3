defbc159f4d4294688c78bc1fac3a5e17521f22a
AlbinHashani <94253491+AlbinHashani@users.noreply.github.com>
2022-05-15 23:18:56 +0200

    Update Base64_Encoder.cs

--- Base64_Encoder.cs
+++ Base64_Encoder.cs
@@ -27,5 +27,27 @@ namespace Base64EncoderDecoder
                 block = (length + padding) / 3;
             }
             length2 = length + padding;
-       
+          }
+
+        public char[] Encode()
+        {
+            byte[] plaintext2;
+            plaintext2 = new byte[length2];
+
+            for (int i = 0; i < length2; i++)
+            {
+                if (i < length)
+                {
+                    plaintext2[i] = plaintext[i];
+                }
+                else
+                {
+                    plaintext2[i] = 0;
+                }
+            }
+
+            byte b1, b2, b3;
+            byte j, j1, j2, j3, j4;
+            byte[] block6b = new byte[block * 4];
+            char[] ciphertext = new char[block * 4];
             
